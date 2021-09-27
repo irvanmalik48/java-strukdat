cded99b3b3bbc2dcd09e6562cad74a1ff29fdb93
Irvan Malik <irvanmalik48@gmail.com>
2021-09-27 10:35:07 +0700

    Waktu: run prettier
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- Waktu/Main.java
+++ Waktu/Main.java
@@ -1,24 +1,31 @@
 class Main {
-    public static void main(String[] args) {
-        Waktu time1 = new Waktu();
-        Waktu time2 = new Waktu(58, 99, 64);
 
-        time1.setJam(20);
-        time1.setMenit(70);
-        time1.setDetik(80);
+	public static void main(String[] args) {
+		Waktu time1 = new Waktu();
+		Waktu time2 = new Waktu(58, 99, 64);
 
-        System.out.println(time1);
-        System.out.println(time2);
+		time1.setJam(20);
+		time1.setMenit(70);
+		time1.setDetik(80);
 
-        System.out.printf("\nHari: %d", time1.getHari());
-        System.out.printf("\nJam: %d", time1.getJam());
-        System.out.printf("\nMenit: %d", time1.getMenit());
-        System.out.printf("\nDetik: %d\n", time1.getDetik());
+		System.out.println(time1);
+		System.out.println(time2);
 
-        System.out.printf("\nTotal detik time1: %d", time1.waktuToDetik());
-        System.out.printf("\nTotal detik time2: %d\n", time2.waktuToDetik());
+		System.out.printf("\nHari: %d", time1.getHari());
+		System.out.printf("\nJam: %d", time1.getJam());
+		System.out.printf("\nMenit: %d", time1.getMenit());
+		System.out.printf("\nDetik: %d\n", time1.getDetik());
 
-        System.out.printf("\nTotal detik time1 tanpa hari: %d", time1.waktuToDetikNoHari());
-        System.out.printf("\nTotal detik time2 tanpa hari: %d\n", time2.waktuToDetikNoHari());
-    }
+		System.out.printf("\nTotal detik time1: %d", time1.waktuToDetik());
+		System.out.printf("\nTotal detik time2: %d\n", time2.waktuToDetik());
+
+		System.out.printf(
+			"\nTotal detik time1 tanpa hari: %d",
+			time1.waktuToDetikNoHari()
+		);
+		System.out.printf(
+			"\nTotal detik time2 tanpa hari: %d\n",
+			time2.waktuToDetikNoHari()
+		);
+	}
 }
--- Waktu/Waktu.java
+++ Waktu/Waktu.java
@@ -1,112 +1,114 @@
 public class Waktu {
-    // atributnya :
-    // int jam 0-23
-    // int menit 0-59
-    // int detik 0-59
 
-    // primitif yg dibutuhkan
-    // simple operation
-    // WaktuToDetik, return nilai detik dari ADT waktu
-    // tambahWaktu
+	// atributnya :
+	// int jam 0-23
+	// int menit 0-59
+	// int detik 0-59
 
-    // Variables
-    private int hari, jam, menit, detik;
+	// primitif yg dibutuhkan
+	// simple operation
+	// WaktuToDetik, return nilai detik dari ADT waktu
+	// tambahWaktu
 
-    // Checking function
-    private void check() {
-        while (this.detik > 59) {
-            this.menit++;
-            this.detik -= 60;
-        }
+	// Variables
+	private int hari, jam, menit, detik;
 
-        while (this.menit > 59) {
-            this.jam++;
-            this.menit -= 60;
-        }
+	// Checking function
+	private void check() {
+		while (this.detik > 59) {
+			this.menit++;
+			this.detik -= 60;
+		}
 
-        while (this.jam > 23) {
-            this.hari++;
-            this.jam -= 24;
-        }
-    }
+		while (this.menit > 59) {
+			this.jam++;
+			this.menit -= 60;
+		}
 
-    // Default Constructor
-    public Waktu() {
-        this.hari = this.jam = this.menit = this.detik = 0;
-    }
+		while (this.jam > 23) {
+			this.hari++;
+			this.jam -= 24;
+		}
+	}
 
-    // Constructor with value
-    public Waktu(int jam, int menit, int detik) {
-        this.hari = 0;
-        this.jam = jam;
-        this.menit = menit;
-        this.detik = detik;
-        check();
-    }
+	// Default Constructor
+	public Waktu() {
+		this.hari = this.jam = this.menit = this.detik = 0;
+	}
 
-    // Getter
-    public int getHari() {
-        return this.hari;
-    }
+	// Constructor with value
+	public Waktu(int jam, int menit, int detik) {
+		this.hari = 0;
+		this.jam = jam;
+		this.menit = menit;
+		this.detik = detik;
+		check();
+	}
 
-    public int getJam() {
-        return this.jam;
-    }
+	// Getter
+	public int getHari() {
+		return this.hari;
+	}
 
-    public int getMenit() {
-        return this.menit;
-    }
+	public int getJam() {
+		return this.jam;
+	}
 
-    public int getDetik() {
-        return this.detik;
-    }
+	public int getMenit() {
+		return this.menit;
+	}
 
-    // Setter
-    public void setJam(int jam) {
-        this.jam = jam;
-        check();
-    }
+	public int getDetik() {
+		return this.detik;
+	}
 
-    public void setMenit(int menit) {
-        this.menit = menit;
-        check();
-    }
+	// Setter
+	public void setJam(int jam) {
+		this.jam = jam;
+		check();
+	}
 
-    public void setDetik(int detik) {
-        this.detik = detik;
-        check();
-    }
-    
-    // tambahWaktu
-    public void tambahWaktu(int jam, int menit, int detik) {
-        this.detik += detik;
-        this.menit += menit;
-        this.jam += jam;
-        check();
-    }
+	public void setMenit(int menit) {
+		this.menit = menit;
+		check();
+	}
 
-    // waktuToDetik
-    public int waktuToDetik() {
-        return (
-            (this.hari * 24 * 60 * 60) + (this.jam * 60 * 60) + (this.menit * 60) + this.detik
-        );
-    }
+	public void setDetik(int detik) {
+		this.detik = detik;
+		check();
+	}
 
-    public int waktuToDetikNoHari() {
-        return (
-            (this.jam * 60 * 60) + (this.menit * 60) + this.detik
-        );
-    }
+	// tambahWaktu
+	public void tambahWaktu(int jam, int menit, int detik) {
+		this.detik += detik;
+		this.menit += menit;
+		this.jam += jam;
+		check();
+	}
 
-    // toString override
-    @Override
-    public String toString() {
-        return String.format(
-            "\nHari: %d\nJam: %d\nMenit: %d\nDetik: %d",
-            hari,
-            jam,
-            menit,
-            detik
-        );
-    }
+	// waktuToDetik
+	public int waktuToDetik() {
+		return (
+			(this.hari * 24 * 60 * 60) +
+			(this.jam * 60 * 60) +
+			(this.menit * 60) +
+			this.detik
+		);
+	}
+
+	public int waktuToDetikNoHari() {
+		return ((this.jam * 60 * 60) + (this.menit * 60) + this.detik);
+	}
+
+	// toString override
+	@Override
+	public String toString() {
+		return String.format(
+			"\nHari: %d\nJam: %d\nMenit: %d\nDetik: %d",
+			hari,
+			jam,
+			menit,
+			detik
+		);
+	}
 }
