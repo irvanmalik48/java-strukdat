8554655288331deffd38d3349d5233edc792d0e0
Irvan Malik <irvanmalik48@gmail.com>
2021-09-27 10:34:06 +0700

    Getting Started: run prettier
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- Getting Started/Main.java
+++ Getting Started/Main.java
@@ -1,13 +1,14 @@
 class Main {
-    public static void main(String[] args) {
-        int arr[] = {1, 2, 3, 4, 5};
-        System.out.print("{");
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i]);
-            if (i < arr.length - 1) {
-                System.out.print(",");
-            }
-        }
-        System.out.print("}");
-    }
+
+	public static void main(String[] args) {
+		int arr[] = { 1, 2, 3, 4, 5 };
+		System.out.print("{");
+		for (int i = 0; i < arr.length; i++) {
+			System.out.print(arr[i]);
+			if (i < arr.length - 1) {
+				System.out.print(",");
+			}
+		}
+		System.out.print("}");
+	}
 }
