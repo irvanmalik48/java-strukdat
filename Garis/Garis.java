f88df0af6346e9ca77127b5f7999eb93a3eec90f
Irvan Malik <irvanmalik48@gmail.com>
2021-09-27 10:33:51 +0700

    Garis: run prettier
    Signed-off-by: Irvan Malik <irvanmalik48@gmail.com>

--- Garis/Garis.java
+++ Garis/Garis.java
@@ -1,113 +1,119 @@
 public class Garis {
-    // atribut
-    private Point awal;
-    private Point akhir;
 
-    private boolean isValidGaris(Point newAwal, Point newAkhir) {
-        return ((newAwal.getX() != newAkhir.getX()) || (newAwal.getY() != newAkhir.getY()));
-    }
+	// atribut
+	private Point awal;
+	private Point akhir;
 
-    // konstruktor default
-    public Garis() {
-        this.awal = new Point();
-        this.akhir = new Point(0, 1);
-    }
+	private boolean isValidGaris(Point newAwal, Point newAkhir) {
+		return (
+			(newAwal.getX() != newAkhir.getX()) ||
+			(newAwal.getY() != newAkhir.getY())
+		);
+	}
 
-    // konstruktor
-    public Garis(Point newAwal, Point newAkhir) {
-        if (isValidGaris(newAwal, newAkhir)) {
-            this.awal = newAwal;
-            this.akhir = newAkhir;
-        } else {
-            System.out.println("Nilai yg dimasukkan tidak valid");
-            this.awal = new Point();
-            this.akhir = new Point(0, 1);
-        }
-    }
+	// konstruktor default
+	public Garis() {
+		this.awal = new Point();
+		this.akhir = new Point(0, 1);
+	}
 
-    // setter
-    public void setAwal(Point awal) {
-        if (isValidGaris(awal, this.akhir)) {
-            this.awal = awal;
-        } else {
-            System.out.println(
-                    "Point awal sama dengan point akhir. Operasi dibatalkan. Nilai dikembalikan ke nilai sebelumnya.");
-        }
-    }
+	// konstruktor
+	public Garis(Point newAwal, Point newAkhir) {
+		if (isValidGaris(newAwal, newAkhir)) {
+			this.awal = newAwal;
+			this.akhir = newAkhir;
+		} else {
+			System.out.println("Nilai yg dimasukkan tidak valid");
+			this.awal = new Point();
+			this.akhir = new Point(0, 1);
+		}
+	}
 
-    public void setAkhir(Point akhir) {
-        if (isValidGaris(this.awal, akhir)) {
-            this.akhir = akhir;
-        } else {
-            System.out.println(
-                    "Point akhir sama dengan point awal. Operasi dibatalkan. Nilai dikembalikan ke nilai sebelumnya.");
-        }
-    }
+	// setter
+	public void setAwal(Point awal) {
+		if (isValidGaris(awal, this.akhir)) {
+			this.awal = awal;
+		} else {
+			System.out.println(
+				"Point awal sama dengan point akhir. Operasi dibatalkan. Nilai dikembalikan ke nilai sebelumnya."
+			);
+		}
+	}
 
-    // getter
-    public Point getAwal() {
-        return this.awal;
-    }
+	public void setAkhir(Point akhir) {
+		if (isValidGaris(this.awal, akhir)) {
+			this.akhir = akhir;
+		} else {
+			System.out.println(
+				"Point akhir sama dengan point awal. Operasi dibatalkan. Nilai dikembalikan ke nilai sebelumnya."
+			);
+		}
+	}
 
-    public Point getAkhir() {
-        return this.akhir;
-    }
+	// getter
+	public Point getAwal() {
+		return this.awal;
+	}
 
-    // toString override
-    @Override
-    public String toString() {
-        return "[" + this.awal + ", " + this.akhir + "]";
-    }
+	public Point getAkhir() {
+		return this.akhir;
+	}
 
-    public void translateX(int x) {
-        this.awal.setX(this.awal.getX() + x);
-        this.akhir.setX(this.akhir.getX() + x);
-    }
+	// toString override
+	@Override
+	public String toString() {
+		return "[" + this.awal + ", " + this.akhir + "]";
+	}
 
-    public void translateY(int y) {
-        this.awal.setY(this.awal.getY() + y);
-        this.akhir.setY(this.akhir.getY() + y);
-    }
+	public void translateX(int x) {
+		this.awal.setX(this.awal.getX() + x);
+		this.akhir.setX(this.akhir.getX() + x);
+	}
 
-    public void translate(int x, int y) {
-        translateX(x);
-        translateY(y);
-    }
+	public void translateY(int y) {
+		this.awal.setY(this.awal.getY() + y);
+		this.akhir.setY(this.akhir.getY() + y);
+	}
 
-    public void rotate90() {
-        this.awal.setX(-this.awal.getX());
-        this.akhir.setX(-this.akhir.getX());
-    }
+	public void translate(int x, int y) {
+		translateX(x);
+		translateY(y);
+	}
 
-    public void rotate180() {
-        rotate90();
-        rotate270();
-    }
+	public void rotate90() {
+		this.awal.setX(-this.awal.getX());
+		this.akhir.setX(-this.akhir.getX());
+	}
 
-    public void rotate270() {
-        this.awal.setY(-this.awal.getY());
-        this.akhir.setY(-this.akhir.getY());
-    }
+	public void rotate180() {
+		rotate90();
+		rotate270();
+	}
 
-    public void mirrorX() {
-        int temp = this.awal.getX();
-        this.awal.setX(this.akhir.getX());
-        this.akhir.setX(temp);
-    }
+	public void rotate270() {
+		this.awal.setY(-this.awal.getY());
+		this.akhir.setY(-this.akhir.getY());
+	}
 
-    public void mirrorY() {
-        int temp = this.awal.getY();
-        this.awal.setY(this.akhir.getY());
-        this.akhir.setY(temp);
-    }
+	public void mirrorX() {
+		int temp = this.awal.getX();
+		this.awal.setX(this.akhir.getX());
+		this.akhir.setX(temp);
+	}
 
-    public void mirrorXY() {
-        mirrorX();
-        mirrorY();
-    }
+	public void mirrorY() {
+		int temp = this.awal.getY();
+		this.awal.setY(this.akhir.getY());
+		this.akhir.setY(temp);
+	}
 
-    public void dilate(int var) {
-        this.akhir.setX(this.akhir.getX() * var);
-        this.akhir.setY(this.akhir.getY() * var);
-    }
+	public void mirrorXY() {
+		mirrorX();
+		mirrorY();
+	}
+
+	public void dilate(int var) {
+		this.akhir.setX(this.akhir.getX() * var);
+		this.akhir.setY(this.akhir.getY() * var);
+	}
 }
--- Garis/Point.java
+++ Garis/Point.java
@@ -1,82 +1,83 @@
 public class Point {
-    private int x;
-    private int y;
 
-    private boolean isValidY(int newy) {
-        return newy >= 0;
-    }
+	private int x;
+	private int y;
 
-    // konstruktor default
-    public Point() {
-        this.x = 0;
-        this.y = 0;
-    }
+	private boolean isValidY(int newy) {
+		return newy >= 0;
+	}
 
-    // konstruktor with value
-    public Point(int newx, int newy) {
-        this.x = newx;
-        if (isValidY(newy)) {
-            this.y = newy;
-        } else {
-            System.out.println("Nilai y yg dimasukkan tidak valid");
-            this.y = 0;
-        }
-    }
+	// konstruktor default
+	public Point() {
+		this.x = 0;
+		this.y = 0;
+	}
 
-    // getter / accessor method
-    public int getX() {
-        return this.x;
-    }
+	// konstruktor with value
+	public Point(int newx, int newy) {
+		this.x = newx;
+		if (isValidY(newy)) {
+			this.y = newy;
+		} else {
+			System.out.println("Nilai y yg dimasukkan tidak valid");
+			this.y = 0;
+		}
+	}
 
-    public int getY() {
-        return this.y;
-    }
+	// getter / accessor method
+	public int getX() {
+		return this.x;
+	}
 
-    // setter / mutator method
-    public void setX(int newX) {
-        this.x = newX;
-    }
+	public int getY() {
+		return this.y;
+	}
 
-    public void setY(int newy) {
-        if (isValidY(newy)) {
-            this.y = newy;
-        } else {
-            System.out.println("Nilai y yg dimasukkan tidak valid");
-        }
-    }
+	// setter / mutator method
+	public void setX(int newX) {
+		this.x = newX;
+	}
 
-    // print
-    public void cetakPoint() {
-        System.out.println("(" + this.x + "," + this.y + ")");
-    }
+	public void setY(int newy) {
+		if (isValidY(newy)) {
+			this.y = newy;
+		} else {
+			System.out.println("Nilai y yg dimasukkan tidak valid");
+		}
+	}
 
-    // print dengan override method toString dari kelas object
+	// print
+	public void cetakPoint() {
+		System.out.println("(" + this.x + "," + this.y + ")");
+	}
 
-    @Override
-    public String toString() {
-        return ("(" + this.x + "," + this.y + ")");
-    }
+	// print dengan override method toString dari kelas object
 
-    // other methods
-    public void mirrorX() {
-        this.x = -1 * this.x;
-    }
+	@Override
+	public String toString() {
+		return ("(" + this.x + "," + this.y + ")");
+	}
 
-    public void mirrorY() {
-        this.y = -1 * this.y;
-    }
+	// other methods
+	public void mirrorX() {
+		this.x = -1 * this.x;
+	}
 
-    public static Point tambahPoint(Point A, Point B) {
-        Point C = new Point();
-        C.x = A.x + B.x;
-        C.y = A.y + B.y;
-        return C;
-    }
+	public void mirrorY() {
+		this.y = -1 * this.y;
+	}
 
-    public Point tambahPoint2(Point B) {
-        Point C = new Point();
-        C.x = this.x + B.x;
-        C.y = this.y + B.y;
-        return C;
-    }
+	public static Point tambahPoint(Point A, Point B) {
+		Point C = new Point();
+		C.x = A.x + B.x;
+		C.y = A.y + B.y;
+		return C;
+	}
+
+	public Point tambahPoint2(Point B) {
+		Point C = new Point();
+		C.x = this.x + B.x;
+		C.y = this.y + B.y;
+		return C;
+	}
 }
